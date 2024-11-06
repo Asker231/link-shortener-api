719cfc362d8d7160af479d0c1fd1bcb5ad0e16c1
Asker <kotsevasker0@gmail.com>
2024-11-06 13:59:03 +0300

    add handlers

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/Asker231/link-shortener-api.git/internal/auth"
+)
+
 func main() {
-	
+	app := http.NewServeMux()
+	auth.NewAuth(app)
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: app,
+	}
+
+	server.ListenAndServe()
 }
---
+++ internal/auth/handler.go
@@ -0,0 +1,19 @@
+package auth
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type AuthLogin struct{}
+
+func (authlogin *AuthLogin) Login() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Login User")
+	}
+}
+
+func NewAuth(router *http.ServeMux) {
+	login := &AuthLogin{}
+	router.HandleFunc("/", login.Login())
+}
